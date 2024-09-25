af12245ffbed78fb2f2647956bfb515219d8f4ec
hklrahmadi <haikalrahmadi@kecilin.id>
2024-09-25 22:43:15 +0700

    initial commit -next js + shadcn -setup prisma also migration to postgre

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -12,6 +13,9 @@ export default function Home() {
           height={38}
           priority
         />
+        <Button>
+          halo ges
+          </Button>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
